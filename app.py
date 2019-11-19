e3f7226710d15e6af256e467e7b76bb18b39d3ef
hectorluz <43251204+hectorluz@users.noreply.github.com>
2019-11-19 10:06:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -75,6 +75,24 @@ trace_2 = go.Scatter(x = [0], y = [0],
 
 fig2 = go.Figure(data = [trace_2], layout = layout)
 
+#fig3.add_trace(go.Scatter(x=k[0], y=(s*(k[0]**a))/k[0],
+#                        mode='lines',
+#                        name='sf(x)'))
+
+
+trace_3 = go.Scatter(x=k[0], y=(s*(k[0]**a))/k[0],
+                        name = 'gx',
+                        line = dict(width = 2,
+                                    color = 'rgb(229, 151, 50)'))
+
+fig3 = go.Figure(data = [trace_3], layout = layout)
+
+
+fig3.add_trace(go.Scatter(x=k[0], y=[d+n]*len(k[0]),
+                        mode='lines',
+                        name='df(x)'))
+
+
 
     # Step 4. Create a Dash layout
 app.layout = html.Div([
@@ -125,7 +143,15 @@ app.layout = html.Div([
                             ], style={'width': '48%', 'display': 'inline-block'}),
                     html.Div([dcc.Graph(id = 'plot2', figure = fig2)],
                         style={'width': '48%', 'float':'right', 'display': 'inline-block'})
+                            ]),
+                html.Div([
+                    html.Div([
+                        dcc.Graph(id = 'plot3', figure = fig3)
+                            ], style={'width': '48%', 'display': 'inline-block'}),
+                    html.Div([dcc.Graph(id = 'plot4', figure = fig2)],
+                        style={'width': '48%', 'float':'right', 'display': 'inline-block'})
                             ])
+                            
                         ])
 
 app.css.append_css({
@@ -134,7 +160,9 @@ app.css.append_css({
 
     # Step 5. Add callback functions
 @app.callback([Output('plot', 'figure'),
-                Output('plot2', 'figure')],
+                Output('plot2', 'figure'),
+                Output('plot3', 'figure'),
+                Output('plot4', 'figure')],
                 [Input('d', 'value'), 
                 Input('n', 'value'),
                 Input('s', 'value'),
@@ -162,7 +190,7 @@ def update_figure(input1,input2,input3,input4):
                                         method="animate",
                                         args=[None])])
                                         ])
-    layout2 = go.Layout(title = 'gk',
+    layout2 = go.Layout(title = 'gy, gk',
                     hovermode = 'closest',
                     updatemenus=[
                             dict(type="buttons",
@@ -171,21 +199,43 @@ def update_figure(input1,input2,input3,input4):
                                         args=[None])])
                                         ])
         
+    layout3 = go.Layout(title = 'Taxas de crescimento',
+                    hovermode = 'closest',
+                    updatemenus=[
+                            dict(type="buttons",
+                            buttons=[dict(label="Play",
+                                        method="animate",
+                                        args=[None])])
+                                        ])
+
+    layout4 = go.Layout(title = 'log(Y/L)',
+                    hovermode = 'closest',
+                    updatemenus=[
+                            dict(type="buttons",
+                            buttons=[dict(label="Play",
+                                        method="animate",
+                                        args=[None])])
+                                        ])
+
     kz = equal
     yz = kz**a
     gk = (s*(kz**(a-1)))-d-n
+    gy = 0
 
     i = 0
 
     time = pd.DataFrame(np.matrix([0,kz,yz,gk]))
 
     for i in range(0,201):
-        gk = s*(time.iloc[i,1]**(a-1))-d-n
+        gk = round(s*(time.iloc[i,1]**(a-1))-d-n,5)
         kz = time.iloc[i,1] + gk
         yz = time.iloc[i,1]**a
-        time = time.append(pd.DataFrame(np.matrix([i,kz,yz,gk])))
+        gy = round(yz - time.iloc[i-1,2],5)
+        #print(gy)
+        print(np.log(yz))
+        time = time.append(pd.DataFrame(np.matrix([i,kz,yz,gk,gy])))
 
-    time.columns = ['idx','kz','yz','gk']
+    time.columns = ['idx','kz','yz','gk','gy']
 
     fig = go.Figure(data = [trace_1],
         layout = layout1
@@ -200,7 +250,33 @@ def update_figure(input1,input2,input3,input4):
     fig2 = go.Figure(data = [trace_2],
         layout = layout2
         )    
-        
+
+    fig2.add_trace(go.Scatter(x=time.iloc[1:,0], y=time.iloc[3:,4],
+                    mode='lines',
+                    name='gy',
+                    line = dict(color = 'blue')))    
+
+    trace_3 = go.Scatter(x=k[0], y=(s*(k[0]**a))/k[0],
+                            name = 'sf(k)/k',
+                            line = dict(width = 2,
+                                        color = 'rgb(229, 151, 50)'))
+
+    fig3 = go.Figure(data = [trace_3], layout = layout3)
+
+
+    fig3.add_trace(go.Scatter(x=k[0], y=[d+n]*len(k[0]),
+                            mode='lines',
+                            name='d + n',
+                            line = dict(color = 'blue')                            ))
+    
+    trace_4 = go.Scatter(x=time.iloc[1:,0], y=np.log(time.iloc[3:,2]),
+                            name = 'gx',
+                            line = dict(width = 2,
+                                        color = 'rgb(229, 151, 50)'))
+
+    fig4 = go.Figure(data = [trace_4], layout = layout4)
+
+
     fig.add_trace(go.Scatter(x=k[0], y=s*(k[0]**a),
                     mode='lines',
                     name='sf(x)'))
@@ -239,11 +315,10 @@ def update_figure(input1,input2,input3,input4):
         ]
     )
     
-    return fig, fig2
+    return fig, fig2, fig3, fig4
+    
+server = app.server
 
-server = app.server  
-  
     # Step 6. Add the server clause
 if __name__ == '__main__':
     app.run_server()
-
